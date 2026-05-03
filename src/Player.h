08f0321e813b934a0c600de2ef8cb25c708ef005
anant-goel <100245626+anant-goel@users.noreply.github.com>
2026-05-03 19:30:50 +0530

    Trying to fix the code

--- src/Player.h
+++ src/Player.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Math.h"
+#include "GameMath.h"
 #include "Physics.h"
 
 enum class PlayerMode { ON_FOOT, IN_VEHICLE };
--- src/Renderer.h
+++ src/Renderer.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Math.h"
+#include "GameMath.h"
 #include "World.h"
 #include <SFML/Graphics.hpp>
 #include <GL/gl.h>
